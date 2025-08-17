595845ed782b1fdfcbcec11d6a615343ef453dbf
Raj Patil <rajpatil3814@gmail.com>
2025-08-17 17:51:45 +0530

    triggring jenkins pipeline

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String [] args )
     {
-        System.out.println("new update..! updated on 13th Aug" );
+        System.out.println("created jenkins pipeline for mvn build" );
     }
 }
